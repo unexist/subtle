292c75de98c81341a4632042c693f7d9fc0ca3be
unexist <unexist@subforge.org>
2017-12-15 13:57:32 +0100

    Shamelessly updated copyright year again

--- src/subtle/screen.c
+++ src/subtle/screen.c
@@ -2,8 +2,8 @@
  /**
   * @package subtle
   *
-  * @file Display functions
-  * @copyright (c) 2005-2013 Christoph Kappel <unexist@subforge.org>
+  * @file Screen functions
+  * @copyright (c) 2005-2017 Christoph Kappel <unexist@subforge.org>
   * @version $Id$
   *
   * This program can be distributed under the terms of the GNU GPLv2.
