9ed254f852830e0f9347052ea9ae6ec96027cb0f
Christoph Kappel <christoph@unexist.dev>
2023-02-22 11:33:25 +0100

    Updated copyright year

--- src/subtle/subtle.c
+++ src/subtle/subtle.c
@@ -3,7 +3,7 @@
   * @package subtle
   *
   * @file Main functions
-  * @copyright (c) 2005-2022 Christoph Kappel <christoph@unexist.dev>
+  * @copyright 2005-present Christoph Kappel <christoph@unexist.dev>
   * @version $Id$
   *
   * This program can be distributed under the terms of the GNU GPLv2.
