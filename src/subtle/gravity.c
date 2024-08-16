925e3a80c4342d958be3d2eca065c88f3dfe065c
Christoph Kappel <christoph@unexist.dev>
2024-08-16 13:56:30 +0200

    Reformatted file

--- src/subtle/gravity.c
+++ src/subtle/gravity.c
@@ -1,175 +1,159 @@
 
- /**
-  * @package subtle
-  *
-  * @file Gravity functions
-  * @copyright 2005-present Christoph Kappel <christoph@unexist.dev>
-  * @version $Id$
-  *
-  * This program can be distributed under the terms of the GNU GPLv2.
-  * See the file COPYING for details.
-  **/
+/**
+ * @package subtle
+ *
+ * @file Gravity functions
+ * @copyright 2005-present Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the GNU GPLv2.
+ * See the file COPYING for details.
+ **/
 
 #include <X11/Xresource.h>
 #include "subtle.h"
 
 /* Method */
 
- /** subGravityNew {{{
-  * @brief Create new gravity
-  * @param[in]  name  Gravity name
-  * @param[in]  geom  Geometry of gravity
-  * @return Returns a new #SubGravity or \p NULL
-  **/
+/** subGravityNew {{{
+ * @brief Create new gravity
+ * @param[in]  name  Gravity name
+ * @param[in]  geom  Geometry of gravity
+ * @return Returns a new #SubGravity or \p NULL
+ **/
 
-SubGravity *
-subGravityNew(const char *name,
-  XRectangle *geom)
-{
-  SubGravity *g = NULL;
+SubGravity *subGravityNew(const char *name, XRectangle *geom) {
+    SubGravity *g = NULL;
 
-  assert(name && geom);
+    assert(name && geom);
 
-  /* Create gravity */
-  g = GRAVITY(subSharedMemoryAlloc(1, sizeof(SubGravity)));
-  g->flags |= SUB_TYPE_GRAVITY;
+    /* Create gravity */
+    g = GRAVITY(subSharedMemoryAlloc(1, sizeof(SubGravity)));
+    g->flags |= SUB_TYPE_GRAVITY;
 
-  if(name) g->quark = XrmStringToQuark(name); ///< Create hash
+    if (name) {
+        g->quark = XrmStringToQuark(name); ///< Create hash
+    }
 
-  /* Sanitize values */
-  g->geom.x      = MINMAX(geom->x,      0, 100);
-  g->geom.y      = MINMAX(geom->y,      0, 100);
-  g->geom.width  = MINMAX(geom->width,  1, 100);
-  g->geom.height = MINMAX(geom->height, 1, 100);
+    /* Sanitize values */
+    g->geom.x = MINMAX(geom->x, 0, 100);
+    g->geom.y = MINMAX(geom->y, 0, 100);
+    g->geom.width = MINMAX(geom->width, 1, 100);
+    g->geom.height = MINMAX(geom->height, 1, 100);
 
-  subSubtleLogDebugSubtle("New: name=%s, quark=%d, x=%d, y=%d,"
-    "width=%d, height=%d\n",
-    name, g->quark, geom->x, geom->y, geom->width, geom->height);
+    subSubtleLogDebugSubtle("New: name=%s, quark=%d, x=%d, y=%d,"
+                            "width=%d, height=%d\n",
+                            name, g->quark, geom->x, geom->y, geom->width, geom->height);
 
-  return g;
+    return g;
 } /* }}} */
 
- /** subGravityGeometry {{{
-  * @brief Calculate geometry of gravity for bounds
-  * @param[in]   g       A #SubGravity
-  * @param[in]   bounds  A #XRectangle
-  * @param[out]  geom    A #XRectangle
-  **/
+/** subGravityGeometry {{{
+ * @brief Calculate geometry of gravity for bounds
+ * @param[in]   g       A #SubGravity
+ * @param[in]   bounds  A #XRectangle
+ * @param[out]  geom    A #XRectangle
+ **/
 
-void
-subGravityGeometry(SubGravity *g,
-  XRectangle *bounds,
-  XRectangle *geom)
-{
-  assert(g && bounds && geom);
+void subGravityGeometry(SubGravity *g, XRectangle *bounds, XRectangle *geom) {
+    assert(g && bounds && geom);
 
-  /* Calculate gravity size for bounds */
-  geom->x      = bounds->x + (bounds->width * g->geom.x / 100);
-  geom->y      = bounds->y + (bounds->height * g->geom.y / 100);
-  geom->width  = (bounds->width * g->geom.width / 100);
-  geom->height = (bounds->height * g->geom.height / 100);
+    /* Calculate gravity size for bounds */
+    geom->x = bounds->x + (bounds->width * g->geom.x / 100);
+    geom->y = bounds->y + (bounds->height * g->geom.y / 100);
+    geom->width = (bounds->width * g->geom.width / 100);
+    geom->height = (bounds->height * g->geom.height / 100);
 } /* }}} */
 
- /** subGravityKill {{{
-  * @brief Kill gravity
-  * @param[in]  g  A #SubGravity
-  **/
+/** subGravityKill {{{
+ * @brief Kill gravity
+ * @param[in]  g  A #SubGravity
+ **/
 
-void
-subGravityKill(SubGravity *g)
-{
-  assert(g);
+void subGravityKill(SubGravity *g) {
+    assert(g);
 
-  free(g);
+    free(g);
 
-  subSubtleLogDebugSubtle("Kill\n");
+    subSubtleLogDebugSubtle("Kill\n");
 } /* }}} */
 
 /* All */
 
- /** subGravityFind {{{
-  * @brief Find gravity
-  * @param[in]   name   Gravity name
-  * @param[in]   quark  Quark
-  * @retval  -1  Not found
-  * @retval  >0  Found id
-  **/
+/** subGravityFind {{{
+ * @brief Find gravity
+ * @param[in]   name   Gravity name
+ * @param[in]   quark  Quark
+ * @retval  -1  Not found
+ * @retval  >0  Found id
+ **/
 
-int
-subGravityFind(const char *name,
-  int quark)
-{
-  int found = -1;
+int subGravityFind(const char *name, int quark) {
+    int found = -1;
 
-  if(0 < subtle->gravities->ndata)
-    {
-      int i, hash = 0;
+    if (0 < subtle->gravities->ndata) {
+        int i, hash = 0;
 
-      /* Get quark */
-      if(name) hash = XrmStringToQuark(name);
-      else hash = quark;
+        /* Get quark */
+        if (name) {
+            hash = XrmStringToQuark(name);
+        } else {
+            hash = quark;
+        }
 
-      for(i = 0; i < subtle->gravities->ndata; i++)
-        {
-          SubGravity *g = GRAVITY(subtle->gravities->data[i]);
+        for (i = 0; i < subtle->gravities->ndata; i++) {
+            SubGravity *g = GRAVITY(subtle->gravities->data[i]);
 
-          /* Compare quarks */
-          if(g->quark == hash)
-            {
-              found = i;
-              break;
+            /* Compare quarks */
+            if (g->quark == hash) {
+                found = i;
+                break;
             }
         }
     }
 
-  return found;
+    return found;
 } /* }}} */
 
- /** subGravityPublish {{{
-  * @brief Publish gravities
-  **/
+/** subGravityPublish {{{
+ * @brief Publish gravities
+ **/
 
-void
-subGravityPublish(void)
-{
-  int i;
-  char **gravities = NULL, buf[30] = { 0 };
-  SubGravity *g = NULL;
+void subGravityPublish(void) {
+    int i;
+    char **gravities = NULL, buf[30] = {0};
+    SubGravity *g = NULL;
 
-  assert(0 < subtle->gravities->ndata);
+    assert(0 < subtle->gravities->ndata);
 
-  /* Alloc space */
-  gravities  = (char **)subSharedMemoryAlloc(subtle->gravities->ndata,
-    sizeof(char *));
+    /* Alloc space */
+    gravities = (char **) subSharedMemoryAlloc(subtle->gravities->ndata, sizeof(char *));
 
-  for(i = 0; i < subtle->gravities->ndata; i++)
-    {
-      g = GRAVITY(subtle->gravities->data[i]);
+    for (i = 0; i < subtle->gravities->ndata; i++) {
+        g = GRAVITY(subtle->gravities->data[i]);
 
-      /* Add gravity to list */
-      snprintf(buf, sizeof(buf), "%dx%d+%d+%d#%s", g->geom.x,
-        g->geom.y, g->geom.width, g->geom.height,
-        XrmQuarkToString(g->quark));
+        /* Add gravity to list */
+        snprintf(buf, sizeof(buf), "%dx%d+%d+%d#%s", g->geom.x, g->geom.y, g->geom.width,
+                 g->geom.height, XrmQuarkToString(g->quark));
 
-      gravities[i] = (char *)subSharedMemoryAlloc(strlen(buf) + 1,
-        sizeof(char));
-      strncpy(gravities[i], buf, strlen(buf));
+        gravities[i] = (char *) subSharedMemoryAlloc(strlen(buf) + 1, sizeof(char));
+        strncpy(gravities[i], buf, strlen(buf));
     }
 
-  /* EWMH: Gravity list and geometries */
-  subSharedPropertySetStrings(subtle->dpy, ROOT,
-    subEwmhGet(SUB_EWMH_SUBTLE_GRAVITY_LIST), gravities,
-    subtle->gravities->ndata);
+    /* EWMH: Gravity list and geometries */
+    subSharedPropertySetStrings(subtle->dpy, ROOT, subEwmhGet(SUB_EWMH_SUBTLE_GRAVITY_LIST),
+                                gravities, subtle->gravities->ndata);
 
-  /* Tidy up */
-  for(i = 0; i < subtle->gravities->ndata; i++)
-    free(gravities[i]);
+    /* Tidy up */
+    for (i = 0; i < subtle->gravities->ndata; i++) {
+        free(gravities[i]);
+    }
 
-  XSync(subtle->dpy, False); ///< Sync all changes
+    XSync(subtle->dpy, False); ///< Sync all changes
 
-  free(gravities);
+    free(gravities);
 
-  subSubtleLogDebugSubtle("Publish: gravities=%d\n", subtle->gravities->ndata);
+    subSubtleLogDebugSubtle("Publish: gravities=%d\n", subtle->gravities->ndata);
 } /* }}} */
 
 // vim:ts=2:bs=2:sw=2:et:fdm=marker
--- src/subtle/hook.c
+++ src/subtle/hook.c
@@ -1,82 +1,71 @@
 
- /**
-  * @package subtle
-  *
-  * @file Hook functions
-  * @copyright 2005-present Christoph Kappel <christoph@unexist.dev>
-  * @version $Id$
-  *
-  * This program can be distributed under the terms of the GNU GPLv2.
-  * See the file COPYING for details.
-  **/
+/**
+ * @package subtle
+ *
+ * @file Hook functions
+ * @copyright 2005-present Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the GNU GPLv2.
+ * See the file COPYING for details.
+ **/
 
 #include "subtle.h"
 
- /** subHookNew {{{
-  * @brief Create new hook
-  * @param[in]  type  Type of hook
-  * @param[in]  proc  Hook proc
-  * @return Returns a new #SubHook or \p NULL
-  **/
+/** subHookNew {{{
+ * @brief Create new hook
+ * @param[in]  type  Type of hook
+ * @param[in]  proc  Hook proc
+ * @return Returns a new #SubHook or \p NULL
+ **/
 
-SubHook *
-subHookNew(int type,
-  unsigned long proc)
-{
-  SubHook *h = NULL;
+SubHook *subHookNew(int type, unsigned long proc) {
+    SubHook *h = NULL;
 
-  assert(proc);
+    assert(proc);
 
-  /* Create new hook */
-  h = HOOK(subSharedMemoryAlloc(1, sizeof(SubHook)));
-  h->flags = (SUB_TYPE_HOOK|type);
-  h->proc  = proc;
+    /* Create new hook */
+    h = HOOK(subSharedMemoryAlloc(1, sizeof(SubHook)));
+    h->flags = (SUB_TYPE_HOOK | type);
+    h->proc = proc;
 
-  subSubtleLogDebugSubtle("new=hook, type=%d, proc=%ld\n", type, proc);
+    subSubtleLogDebugSubtle("new=hook, type=%d, proc=%ld\n", type, proc);
 
-  return h;
+    return h;
 } /* }}} */
 
- /** subHookCall {{{
-  * @brief Emit a hook
-  * @param[in]  type  Type of hook
-  * @param[in]  data  Hook data
-  **/
+/** subHookCall {{{
+ * @brief Emit a hook
+ * @param[in]  type  Type of hook
+ * @param[in]  data  Hook data
+ **/
 
-void
-subHookCall(int type,
-  void *data)
-{
-  int i;
+void subHookCall(int type, void *data) {
+    int i;
 
-  /* Call matching hooks */
-  for(i = 0; i < subtle->hooks->ndata; i++)
-    {
-      SubHook *h = HOOK(subtle->hooks->data[i]);
+    /* Call matching hooks */
+    for (i = 0; i < subtle->hooks->ndata; i++) {
+        SubHook *h = HOOK(subtle->hooks->data[i]);
 
-      if((h->flags & ~SUB_TYPE_HOOK) == type)
-        {
-          subRubyCall(SUB_CALL_HOOKS, h->proc, data);
+        if ((h->flags & ~SUB_TYPE_HOOK) == type) {
+            subRubyCall(SUB_CALL_HOOKS, h->proc, data);
 
-          subSubtleLogDebug("call=hook, type=%d, proc=%ld, data=%p\n",
-            type, h->proc, data);
+            subSubtleLogDebug("call=hook, type=%d, proc=%ld, data=%p\n", type, h->proc, data);
         }
     }
 } /* }}} */
 
- /** subHookKill {{{
-  * @brief Kill a hook
-  * @param[in]  h  A #SubHook
-  **/
+/** subHookKill {{{
+ * @brief Kill a hook
+ * @param[in]  h  A #SubHook
+ **/
 
-void
-subHookKill(SubHook *h)
-{
-  assert(h);
+void subHookKill(SubHook *h) {
+    assert(h);
 
-  free(h);
+    free(h);
 
-  subSubtleLogDebugSubtle("kill=hook\n");
+    subSubtleLogDebugSubtle("kill=hook\n");
 } /* }}} */
 
 // vim:ts=2:bs=2:sw=2:et:fdm=marker
